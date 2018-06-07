b2047b4d1d52dcffda678793733e8cd31f0ced46
fantasts <fantasts@excite.com>
2018-06-07 12:02:05 -0400

    Committing test comments

--- Source/Domain/Vcc.SocialNetwork.Domain.Model/AppContext.cs
+++ Source/Domain/Vcc.SocialNetwork.Domain.Model/AppContext.cs
@@ -10,7 +10,7 @@ namespace Vcc.SocialNetwork.Domain.Model
         static AppContext()
         {
             //Database.SetInitializer(new DropCreateDatabaseIfModelChanges<AppContext>());
-
+            // Test
         }
 
         public DbSet<Member> Members { get; set; }
